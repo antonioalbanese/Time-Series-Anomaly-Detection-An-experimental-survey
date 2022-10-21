4d7e3393ab0cde5b6155a2eed571cf2e442be42b
antonioalbanese <antonio.albanese@studenti.polito.it>
2022-10-21 12:10:23 +0200

    added ADMethod class

---
+++ AnomalyDetectionMethodClass.py
@@ -0,0 +1,31 @@
+import os
+import argparse
+
+from torch.backends import cudnn
+from transformer.utils.utils import *
+
+from transformer.solver import Solver
+
+
+class ADMethod():
+	def __init__(self, method_name:str, settings: dict, data_path: str):
+		self.method_name = method_name
+		self.settings = settings
+		self.data_path = data_path
+
+	def prepare_pipeline(self):
+		if self.method_name == 'transformer':
+			self.solver = Solver(vars(config))
+
+
+	def train(self):
+		if self.method_name == 'transformer':
+			self.solver.train()
+
+
+	def test(self):
+		if self.method_name == 'transformer':
+			self.solver.test()
+
+	def results(self):
+		pass
