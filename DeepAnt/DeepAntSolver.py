4c6c9a5de75ef23c34a8ba9fb7c6a527e427397d
antonioalbanese <74200967+antonioalbanese@users.noreply.github.com>
2022-10-25 18:39:59 +0200

    Update DeepAntSolver.py

--- DeepAnt/DeepAntSolver.py
+++ DeepAnt/DeepAntSolver.py
@@ -42,6 +42,5 @@ class Solver(object):
     
     def test(self):
         output = self.trainer.predict(self.anomaly_detector, self.dm)
-        print(output)
-        ## TODO: look at what it does return
-        return output.copy()
+        o = output.copy()
+        return o
