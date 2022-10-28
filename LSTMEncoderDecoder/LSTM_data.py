c85ce4e6e6039caaf5ab34cfbbca9a6e9b492132
antonioalbanese <74200967+antonioalbanese@users.noreply.github.com>
2022-10-28 17:42:09 +0200

    sklearn

--- LSTMEncoderDecoder/LSTM_data.py
+++ LSTMEncoderDecoder/LSTM_data.py
@@ -7,6 +7,7 @@ import numpy as np
 from pathlib import Path
 import pickle
 from torch.utils.data import Dataset
+from sklearn.preprocessing import MinMaxScaler
 
 class LSTMDataset(Dataset):
     def __init__(self, df, seq_len):
