eb6148d2c442f9a18e50676055a9ea46225ac00f
antonioalbanese <74200967+antonioalbanese@users.noreply.github.com>
2022-12-01 23:53:46 +0100

    fix bug

--- AnomalyDetectionMethodClass.py
+++ AnomalyDetectionMethodClass.py
@@ -202,7 +202,7 @@ class ADMethod():
 			self.model.to(self.device)
 			self.model.train()
 			for epoch in range(self.config['EPOCHS']):
-				epoch_loss1, epoch_loss2 = TransformerEpoch(self.optimizer, self.train_dl, self.config['K'], self.config['SEQ_LEN'], self.device)
+				epoch_loss1, epoch_loss2 = TransformerEpoch(self.criterion, self.optimizer, self.train_dl, self.config['K'], self.config['SEQ_LEN'], self.device)
 				if self.config['LOGGER']:
 					wandb.define_metric("epoch")
 					wandb.define_metric("loss1", step_metric="epoch")
